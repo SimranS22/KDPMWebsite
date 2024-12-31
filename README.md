24332aadfb5cbce554d8d7b0d2fa329ab04dd2a3
Simran Sharma <103310345+SimranS22@users.noreply.github.com>
2024-12-31 08:46:08 +0000

    added website files

--- README.md
+++ README.md
@@ -1,13 +1,51 @@
-# GitHub Codespaces ♥️ Flask
+# Chronic Kidney Disease Prediction Website 🩺
 
-Welcome to your shiny new Codespace running Flask! We've got everything fired up and running for you to explore Flask.
+Welcome to the **Chronic Kidney Disease Prediction Website**! This project uses **Flask**, **Python**, **HTML**, and **CSS**, hosted with **GitHub Codespaces**, to create a simple yet powerful web application designed to predict the likelihood of Chronic Kidney Disease (CKD).
 
-You've got a blank canvas to work on from a git perspective as well. There's a single initial commit with the what you're seeing right now - where you go from here is up to you!
+The website provides an intuitive interface for users to input their health parameters, and it uses a machine learning model to predict the likelihood of CKD.
 
-Everything you do here is contained within this one codespace. There is no repository on GitHub yet. If and when you’re ready you can click "Publish Branch" and we’ll create your repository and push up your project. If you were just exploring then and have no further need for this code then you can simply delete your codespace and it's gone forever.
+For more details about the model and development process, you can refer to the following GitHub repository: [Chronic Kidney Disease Prediction Model](https://github.com/meAnubhav/Kidney-Disease-Prediction-Model).
 
-To run this application:
+### Demo
 
-```
-flask --debug run
-```
+Check out the demo here: [Live Demo](https://github.com/user-attachments/assets/85eb07c6-df5a-4bf2-bbbc-b7b48ffa3fd8)
+
+## 🛠 **Technologies Used**
+- **Flask**: A lightweight web framework for Python to build the backend of the application.
+- **Python**: The programming language used to power the logic and machine learning model.
+- **HTML & CSS**: For building a responsive and user-friendly web interface.
+- **GitHub Codespaces**: A cloud-powered development environment that simplifies building and hosting web applications.
+
+## 🚀 **Getting Started with Flask in GitHub Codespaces**
+GitHub Codespaces ♥️ Flask enables an easy setup to create and deploy Flask applications. This project demonstrates how these technologies can be used to develop a website for Chronic Kidney Disease prediction.
+
+### Features
+- **Prediction Model**: Uses a machine learning model to assess the likelihood of Chronic Kidney Disease based on user input.
+- **Flask Framework**: Powers the backend of the application for smooth and efficient functionality.
+- **GitHub Codespaces**: Simplifies the development and hosting of the project in a cloud-based environment.
+
+## 🛠 **How to Run This Project**
+
+1. **Open in GitHub Codespaces**:
+   Clone the repository or start a new Codespace from your GitHub account.
+
+2. **Set Up Dependencies**:
+   Install the required Python packages by running:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. **Run the Flask Application**:
+   Start the Flask server using the following command:
+   ```bash
+   flask --debug run
+   ```
+   The application will be available at `http://127.0.0.1:5000`.
+
+4. **Predict Chronic Kidney Disease**:
+   Input the health data in the web interface to get a prediction on the likelihood of CKD.
+
+## 🏥 **Disclaimer**
+This tool provides predictions based on a machine learning model and is meant for informational purposes only. It should not be used as a substitute for professional medical advice, diagnosis, or treatment. Please consult with a healthcare professional if you have any concerns.
+
+Happy coding! 😊
--- app.py
+++ app.py
@@ -1,7 +1,127 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
+
+from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+df = pd.read_csv('encoded.csv')
+
+X = df.drop(['target'],axis=1)
+Y = df['target']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train , X_test, Y_train, Y_test = train_test_split(X_scaled,Y,test_size=0.30,random_state=0)
+
+gb = GradientBoostingClassifier()
+gb.fit(X_train, Y_train)
+# gb_pred = gb.predict(X_test)
+
+
+@app.route('/')
+def man():
+    return render_template('index.html')
+
+@app.route('/back', methods=['GET'])
+def back():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def get_form_data():
+    Name = request.form.get('Name')
+    Age = request.form.get('Age')
+    BloodPressure = request.form.get('BloodPressure')
+    SpecificGravity = request.form.get('SpecificGravity')
+    Albumin = request.form.get('Albumin')
+    Sugar = request.form.get('Sugar')
+    RedBloodCells = request.form.get('RedBloodCells')
+    PusCell = request.form.get('PusCell')
+    PusCellClumps = request.form.get('PusCellClumps')
+    Bacteria = request.form.get('Bacteria')
+    BloodGlucoseRandom = request.form.get('BloodGlucoseRandom')
+    BloodUrea = request.form.get('BloodUrea')
+    SerumCreatinine = request.form.get('SerumCreatinine')
+    Sodium = request.form.get('Sodium')
+    Potassium = request.form.get('Potassium')
+    Haemoglobin = request.form.get('Haemoglobin')
+    PackedCellVolume = request.form.get('PackedCellVolume')
+    WhiteBloodCellCount = request.form.get('WhiteBloodCellCount')
+    RedBloodCellCount = request.form.get('RedBloodCellCount')
+    Hypertension = request.form.get('Hypertension')
+    DiabetesMellitus = request.form.get('DiabetesMellitus')
+    CoronaryArteryDisease = request.form.get('CoronaryArteryDisease')
+    Appetite = request.form.get('Appetite')
+    PedalEdema = request.form.get('PedalEdema')
+    Anaemia = request.form.get('Anaemia')
+
+    # Standardization function
+    def standardize(x, mean, standard_deviation):
+        return (float(x) - float(mean)) / float(standard_deviation)
+
+    # Standardize numerical features
+    age = standardize(Age, 51.852792, 16.642435)
+    blood_pressure = standardize(BloodPressure, 76.649746, 12.187850)  # You need to define mean_bp and std_bp
+    blood_glucose_random = standardize(BloodGlucoseRandom, 144.319797, 75.023655) 
+    specific_gravity = standardize(SpecificGravity,1.017754,0.005430)
+    albumin_lvl = standardize(Albumin,0.885787,1.297738)
+    sugar_lvl = standardize(Sugar,0.390863,1.031078)
+    blood_urea_lvl = standardize(BloodUrea,56.637563,49.212389)
+    serum_creatinine_lvl = standardize(SerumCreatinine,2.997589,5.649333)
+    sodium_lvl = standardize(Sodium,137.755076,9.113033)
+    potassium_lvl = standardize(Potassium,4.590102,2.839906)
+    haemoglobin_lvl = standardize(Haemoglobin,12.557868,2.731251)
+    packed_cell_volume = standardize(PackedCellVolume,39.126904,8.204395)
+    white_blood_cell_count = standardize(WhiteBloodCellCount,8280.964467,2499.275841)
+    red_blood_cell_count = standardize(RedBloodCellCount,4.744416,0.843344)
+
+
+    # Convert categorical features to binary
+    pus_cell = 1 if PusCell == "normal" else 0
+    pus_cell_clumps = 1 if PusCellClumps == "present" else 0
+    rbc = 1 if RedBloodCells == "normal" else 0 
+    bacteria = 1 if Bacteria == "present" else 0
+    hypertension = 1 if Hypertension == "yes" else 0
+    dm = 1 if DiabetesMellitus == "yes" else 0
+    cad = 1 if CoronaryArteryDisease == "yes" else 0
+    appetite = 1 if Appetite == "poor" else 0
+    pedaledema = 1 if PedalEdema == "yes" else 0
+    anaemia = 1 if Anaemia == "yes" else 0
+
+    # Create input DataFrame
+    input_df = pd.DataFrame({
+    'age': [float(age)],
+    'blood_pressure': [float(blood_pressure)],
+    'specific_gravity': [float(specific_gravity)],
+    'albumin_lvl': [float(albumin_lvl)],
+    'sugar_lvl': [float(sugar_lvl)],
+    'red_blood_cells': [float(rbc)],
+    'pus_cell': [float(pus_cell)],
+    'pus_cell_clumps': [float(pus_cell_clumps)],
+    'bacteria': [float(bacteria)],
+    'blood_glucose_random_lvl': [float(blood_glucose_random)],
+    'blood_urea_lvl': [float(blood_urea_lvl)],
+    'serum_creatinine_lvl': [float(serum_creatinine_lvl)],
+    'sodium_lvl': [float(sodium_lvl)],
+    'potassium_lvl': [float(potassium_lvl)],
+    'haemoglobin_lvl': [float(haemoglobin_lvl)],
+    'packed_cell_volume': [float(packed_cell_volume)],
+    'white_blood_cell_count': [float(white_blood_cell_count)],
+    'red_blood_cell_count': [float(red_blood_cell_count)],
+    'hypertension': [float(hypertension)],
+    'diabetes_mellitus': [float(dm)],
+    'coronary_artery_disease': [float(cad)],
+    'appetite': [float(appetite)],
+    'peda_edema': [float(pedaledema)],
+    'anaemia': [float(anaemia)]
+    })
+
+    # Make predictions (ckd=0, notckd=1)
+    result = gb.predict_proba(input_df)
+    yes_ckd = round(float(result[0][0] * 100), 2)
+    no_ckd = round(float(result[0][1] * 100), 2)
+    return render_template('output.html', Name=Name, no_ckd=no_ckd, yes_ckd=yes_ckd)
